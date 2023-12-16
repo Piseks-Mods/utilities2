b029d8b9adf7112e5c412926237d8805c43186b9
Písek Pískovec <pisekpiskovec@gmail.com>
2023-12-16 20:30:58 +0100

    Copper Rod is sword type

--- src/main/java/cz/pisekpiskovec/piseksutilities/item/CopperRodItem.java
+++ src/main/java/cz/pisekpiskovec/piseksutilities/item/CopperRodItem.java
@@ -4,16 +4,12 @@ package cz.pisekpiskovec.piseksutilities.item;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.World;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.Attribute;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.IItemTier;
 import net.minecraft.entity.Entity;
-import net.minecraft.block.BlockState;
 
 import java.util.stream.Stream;
 import java.util.Map;
@@ -23,9 +19,6 @@ import java.util.AbstractMap;
 import cz.pisekpiskovec.piseksutilities.procedures.CopperRodLockProcedure;
 import cz.pisekpiskovec.piseksutilities.itemgroup.PUiiToolsItemGroup;
 import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiModElements;
-
-import com.google.common.collect.Multimap;
-import com.google.common.collect.ImmutableMultimap;
 
 @PiseksUtilitiesIiModElements.ModElement.Tag
 public class CopperRodItem extends PiseksUtilitiesIiModElements.ModElement {
@@ -38,7 +31,31 @@ public class CopperRodItem extends PiseksUtilitiesIiModElements.ModElement {
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new ItemToolCustom() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 128;
+			}
+
+			public float getEfficiency() {
+				return 5f;
+			}
+
+			public float getAttackDamage() {
+				return 3f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 10;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(CopperIngotItem.block));
+			}
+		}, 3, -1f, new Item.Properties().group(PUiiToolsItemGroup.tab)) {
 			@Override
 			public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
 				super.inventoryTick(itemstack, world, entity, slot, selected);
@@ -51,47 +68,5 @@ public class CopperRodItem extends PiseksUtilitiesIiModElements.ModElement {
 							(_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 			}
 		}.setRegistryName("copper_rod"));
-	}
-
-	private static class ItemToolCustom extends Item {
-		protected ItemToolCustom() {
-			super(new Item.Properties().group(PUiiToolsItemGroup.tab).maxDamage(128));
-		}
-
-		@Override
-		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
-			return 1;
-		}
-
-		@Override
-		public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
-			stack.damageItem(1, entityLiving, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
-			return true;
-		}
-
-		@Override
-		public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-			stack.damageItem(2, attacker, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
-			return true;
-		}
-
-		@Override
-		public int getItemEnchantability() {
-			return 10;
-		}
-
-		@Override
-		public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
-			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
-				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-				builder.putAll(super.getAttributeModifiers(equipmentSlot));
-				builder.put(Attributes.ATTACK_DAMAGE,
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 3f, AttributeModifier.Operation.ADDITION));
-				builder.put(Attributes.ATTACK_SPEED,
-						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -1, AttributeModifier.Operation.ADDITION));
-				return builder.build();
-			}
-			return super.getAttributeModifiers(equipmentSlot);
-		}
 	}
 }
