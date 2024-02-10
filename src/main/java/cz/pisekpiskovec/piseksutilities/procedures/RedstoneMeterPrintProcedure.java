707118c3585fbf74f4e81663dc9122e522aa9229
Písek Pískovec <pisekpiskovec@gmail.com>
2024-02-10 23:04:25 +0100

    Redstone Meter
    new experimental

---
+++ src/main/java/cz/pisekpiskovec/piseksutilities/procedures/RedstoneMeterPrintProcedure.java
@@ -0,0 +1,55 @@
+package cz.pisekpiskovec.piseksutilities.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiMod;
+
+public class RedstoneMeterPrintProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency world for procedure RedstoneMeterPrint!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency x for procedure RedstoneMeterPrint!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency y for procedure RedstoneMeterPrint!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency z for procedure RedstoneMeterPrint!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency entity for procedure RedstoneMeterPrint!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+			((PlayerEntity) entity)
+					.sendStatusMessage(
+							new StringTextComponent((new java.text.DecimalFormat("##")
+									.format((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0))),
+							(true));
+		}
+	}
+}
