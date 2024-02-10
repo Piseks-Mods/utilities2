a5716b18531f88d181a002d137b3018515d47f16
Písek Pískovec <pisekpiskovec@gmail.com>
2024-02-10 18:18:50 +0100

    Removed Lava spawning

--- src/main/java/cz/pisekpiskovec/piseksutilities/block/AmberLampBlock.java
+++ src/main/java/cz/pisekpiskovec/piseksutilities/block/AmberLampBlock.java
@@ -7,15 +7,12 @@ import net.minecraftforge.common.ToolType;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.World;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
@@ -23,14 +20,9 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
-import java.util.stream.Stream;
-import java.util.Map;
 import java.util.List;
-import java.util.HashMap;
 import java.util.Collections;
-import java.util.AbstractMap;
 
-import cz.pisekpiskovec.piseksutilities.procedures.AmberLampDestroyedProcedure;
 import cz.pisekpiskovec.piseksutilities.itemgroup.PUiiDecorationsItemGroup;
 import cz.pisekpiskovec.piseksutilities.item.AmberItem;
 import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiModElements;
@@ -75,20 +67,6 @@ public class AmberLampBlock extends PiseksUtilitiesIiModElements.ModElement {
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(AmberItem.block, (int) (3)));
-		}
-
-		@Override
-		public boolean removedByPlayer(BlockState blockstate, World world, BlockPos pos, PlayerEntity entity, boolean willHarvest, FluidState fluid) {
-			boolean retval = super.removedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
-
-			AmberLampDestroyedProcedure.executeProcedure(Stream
-					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
-							new AbstractMap.SimpleEntry<>("z", z), new AbstractMap.SimpleEntry<>("entity", entity))
-					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
-			return retval;
 		}
 	}
 }
