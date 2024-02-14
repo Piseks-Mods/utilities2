ff448a5d285b160286a209b21eb149e1e58aff37
Písek Pískovec <pisekpiskovec@gmail.com>
2024-02-14 12:46:57 +0100

    multiple
    Darkened Glass
    Launcher
    Release 2.0a6

---
+++ src/main/java/cz/pisekpiskovec/piseksutilities/procedures/LauncherLaunchProcedure.java
@@ -0,0 +1,107 @@
+package cz.pisekpiskovec.piseksutilities.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiMod;
+
+public class LauncherLaunchProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency world for procedure LauncherLaunch!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency x for procedure LauncherLaunch!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency y for procedure LauncherLaunch!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency z for procedure LauncherLaunch!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency entity for procedure LauncherLaunch!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0) != 0) {
+			entity.setMotion((entity.getMotion().getX()), ((new Object() {
+				public double getValue(IWorld world, BlockPos pos, String tag) {
+					TileEntity tileEntity = world.getTileEntity(pos);
+					if (tileEntity != null)
+						return tileEntity.getTileData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, new BlockPos(x, y, z), "launcherStrength"))
+					/ ((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0)),
+					(entity.getMotion().getZ()));
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos(x, y, z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.explode")),
+						SoundCategory.BLOCKS, (float) ((new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength"))
+								/ ((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0)),
+						(float) ((new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength"))
+								/ ((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0)));
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.explode")),
+						SoundCategory.BLOCKS, (float) ((new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength"))
+								/ ((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0)),
+						(float) ((new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength"))
+								/ ((world instanceof World) ? ((World) world).getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) : 0)),
+						false);
+			}
+		}
+	}
+}
---
+++ src/main/java/cz/pisekpiskovec/piseksutilities/procedures/LauncherStrenghtConfigProcedure.java
@@ -0,0 +1,133 @@
+package cz.pisekpiskovec.piseksutilities.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+
+import java.util.Map;
+
+import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiMod;
+
+public class LauncherStrenghtConfigProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency world for procedure LauncherStrenghtConfig!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency x for procedure LauncherStrenghtConfig!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency y for procedure LauncherStrenghtConfig!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency z for procedure LauncherStrenghtConfig!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				PiseksUtilitiesIiMod.LOGGER.warn("Failed to load dependency entity for procedure LauncherStrenghtConfig!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity.isSneaking()) {
+			if (new Object() {
+				public double getValue(IWorld world, BlockPos pos, String tag) {
+					TileEntity tileEntity = world.getTileEntity(pos);
+					if (tileEntity != null)
+						return tileEntity.getTileData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, new BlockPos(x, y, z), "launcherStrength") >= 17) {
+				if (!world.isRemote()) {
+					BlockPos _bp = new BlockPos(x, y, z);
+					TileEntity _tileEntity = world.getTileEntity(_bp);
+					BlockState _bs = world.getBlockState(_bp);
+					if (_tileEntity != null)
+						_tileEntity.getTileData().putDouble("launcherStrength", ((new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength")) - 1));
+					if (world instanceof World)
+						((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+				}
+			} else {
+				if (!world.isRemote()) {
+					BlockPos _bp = new BlockPos(x, y, z);
+					TileEntity _tileEntity = world.getTileEntity(_bp);
+					BlockState _bs = world.getBlockState(_bp);
+					if (_tileEntity != null)
+						_tileEntity.getTileData().putDouble("launcherStrength", 64);
+					if (world instanceof World)
+						((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+				}
+			}
+		} else {
+			if (new Object() {
+				public double getValue(IWorld world, BlockPos pos, String tag) {
+					TileEntity tileEntity = world.getTileEntity(pos);
+					if (tileEntity != null)
+						return tileEntity.getTileData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, new BlockPos(x, y, z), "launcherStrength") <= 63) {
+				if (!world.isRemote()) {
+					BlockPos _bp = new BlockPos(x, y, z);
+					TileEntity _tileEntity = world.getTileEntity(_bp);
+					BlockState _bs = world.getBlockState(_bp);
+					if (_tileEntity != null)
+						_tileEntity.getTileData().putDouble("launcherStrength", (new Object() {
+							public double getValue(IWorld world, BlockPos pos, String tag) {
+								TileEntity tileEntity = world.getTileEntity(pos);
+								if (tileEntity != null)
+									return tileEntity.getTileData().getDouble(tag);
+								return -1;
+							}
+						}.getValue(world, new BlockPos(x, y, z), "launcherStrength") + 1));
+					if (world instanceof World)
+						((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+				}
+			} else {
+				if (!world.isRemote()) {
+					BlockPos _bp = new BlockPos(x, y, z);
+					TileEntity _tileEntity = world.getTileEntity(_bp);
+					BlockState _bs = world.getBlockState(_bp);
+					if (_tileEntity != null)
+						_tileEntity.getTileData().putDouble("launcherStrength", 16);
+					if (world instanceof World)
+						((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+				}
+			}
+		}
+		if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+			((PlayerEntity) entity).sendStatusMessage(new StringTextComponent(("Strength: " + new java.text.DecimalFormat("##").format(new Object() {
+				public double getValue(IWorld world, BlockPos pos, String tag) {
+					TileEntity tileEntity = world.getTileEntity(pos);
+					if (tileEntity != null)
+						return tileEntity.getTileData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, new BlockPos(x, y, z), "launcherStrength")))), (true));
+		}
+	}
+}
