b9fcd9c065fce9bae27da89de0df06f2b611d160
Písek Pískovec <pisekpiskovec@gmail.com>
2024-02-29 21:46:07 +0100

    Remove Titanium Chunk convert description

--- src/main/java/cz/pisekpiskovec/piseksutilities/item/TitaniumChunkItem.java
+++ src/main/java/cz/pisekpiskovec/piseksutilities/item/TitaniumChunkItem.java
@@ -3,16 +3,10 @@ package cz.pisekpiskovec.piseksutilities.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.block.BlockState;
-
-import java.util.List;
 
 import cz.pisekpiskovec.piseksutilities.itemgroup.PUiiMiscItemGroup;
 import cz.pisekpiskovec.piseksutilities.PiseksUtilitiesIiModElements;
@@ -45,12 +39,6 @@ public class TitaniumChunkItem extends PiseksUtilitiesIiModElements.ModElement {
 		@Override
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
-		}
-
-		@Override
-		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-			super.addInformation(itemstack, world, list, flag);
-			list.add(new StringTextComponent("\u00A76Combine 9 Copper Chunks to get 1 Titanium Chunk"));
 		}
 	}
 }
